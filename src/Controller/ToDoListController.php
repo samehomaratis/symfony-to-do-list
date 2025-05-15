505961dcdaef55223516b5c64abc08ac58da48a7
Sameh Mohamed Ahmed Omr <samehmohamedomar22@gmail.com>
2025-05-15 15:42:08 +0300

    add new project

---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ToDoListController extends AbstractController
+{
+    #[Route('/to/do/list', name: 'app_to_do_list')]
+    public function index(): Response
+    {
+        return $this->render('to_do_list/index.html.twig', [
+            'controller_name' => 'ToDoListController',
+        ]);
+    }
+}
