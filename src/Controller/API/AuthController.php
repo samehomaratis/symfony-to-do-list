56e4f8b065b9989de287b32a0fcb16b90599e830
Sameh Mohamed Ahmed Omr <samehmohamedomar22@gmail.com>
2025-06-02 15:57:14 +0300

    register api

--- src/Controller/API/AuthController.php
+++ src/Controller/API/AuthController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller\API;
 
+use App\Entity\UserModal;
+use App\Form\RegistrationFormTypeForm;
 use App\Repository\UserModalRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,5 +41,40 @@ class AuthController extends AbstractController
         $token = $jwtManager->create($user);
 
         return new JsonResponse(['token' => $token]);
+    }
+
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(Request $request,
+                             UserPasswordHasherInterface $passwordHasher,
+                             JWTTokenManagerInterface $jwtManager,
+                             EntityManagerInterface $em): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        if (!$data || !isset($data['email'], $data['name'] , $data['password'])) {
+            return new JsonResponse(['error' => 'Name, email and password are required'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $user = new UserModal();
+        $form = $this->createForm(RegistrationFormTypeForm::class, $user, [
+            'csrf_protection' => false,
+        ]);
+
+        $form->submit($data);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
+
+            $em->persist($user);
+            $em->flush();
+
+            $token = $jwtManager->create($user);
+
+            return new JsonResponse(['token' => $token]);
+        }
+
+        return new JsonResponse([
+            'errors' => (string)$form->getErrors(true, false)
+        ], JsonResponse::HTTP_BAD_REQUEST);
     }
 }
